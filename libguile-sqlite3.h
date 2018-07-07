30a055c369ac28e3ed9c77720fe1909bc5cd3476
Andrew Shen <Andrew.Y.Shen@jacobs.ucsd.edu>
2018-07-07 11:58:19 -0700

    Include license headers in each source file

--- libguile-sqlite3.h
+++ libguile-sqlite3.h
@@ -1,3 +1,21 @@
+/*
+This file is part of libguile-sqlite3.
+
+libguile-sqlite3 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libguile-sqlite3 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libguile-sqlite3.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef LIBGUILE_SQLITE3_H
 #define LIBGUILE_SQLITE3_H
 
